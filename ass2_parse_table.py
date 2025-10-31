4e9687d4d348acc229e2ae51691c465c55f1eea3
Paul Pavlovic <esteficek1@outlook.com>
2025-10-31 16:31:37 +1100

    Update ass2_parse_table.py

--- ass2_parse_table.py
+++ ass2_parse_table.py
@@ -1,55 +1,90 @@
-# LL(1) parse table for a simple arithmetic grammar:
-# Grammar (classic expression grammar):
-#   E  -> T E'
-#   E' -> + T E' | - T E' | ε
-#   T  -> F T'
-#   T' -> * F T' | / F T' | ε
-#   F  -> ( E ) | NUMBER | IDENTIFIER
-#
-# Terminals used as keys: '+', '-', '*', '/', '(', ')', 'NUMBER', 'IDENTIFIER', '$' (EOF)
-# Nonterminals: 'E', "E'", 'T', "T'", 'F'
-# Productions are lists of symbols (terminals or nonterminals). Epsilon is represented by an empty list [].
+from ass2_lexer import TokenType, Token, Lexer
 
-EPSILON = []
+class ASTNode:
+    pass
 
-NONTERMINALS = ["E", "E'", "T", "T'", "F"]
-TERMINALS = ['+', '-', '*', '/', '(', ')', 'NUMBER', 'IDENTIFIER', '$']
+class NumberNode(ASTNode):
+    def __init__(self, value):
+        self.value = value
+    
+    def __repr__(self):
+        return f'NumberNode({self.value})'
 
-PARSE_TABLE = {
-    # E
-    ('E', 'NUMBER'): ['T', "E'"],
-    ('E', 'IDENTIFIER'): ['T', "E'"],
-    ('E', '('): ['T', "E'"],
+class IdentifierNode(ASTNode):
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return f'IdentifierNode({self.name})'
 
-    # E'
-    ("E'", '+'): ['+', 'T', "E'"],
-    ("E'", '-'): ['-', 'T', "E'"],
-    ("E'", ')'): EPSILON,
-    ("E'", '$'): EPSILON,
+class StringNode(ASTNode):
+    def __init__(self, value):
+        self.value = value
 
-    # T
-    ('T', 'NUMBER'): ['F', "T'"],
-    ('T', 'IDENTIFIER'): ['F', "T'"],
-    ('T', '('): ['F', "T'"],
+    def __repr__(self):
+        return f'StringNode({self.value})'
 
-    # T'
-    ("T'", '*'): ['*', 'F', "T'"],
-    ("T'", '/'): ['/', 'F', "T'"],
-    ("T'", '+'): EPSILON,
-    ("T'", '-'): EPSILON,
-    ("T'", ')'): EPSILON,
-    ("T'", '$'): EPSILON,
+class BinaryOpNode(ASTNode):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+    
+    def __repr__(self):
+        return f'BinaryOpNode({self.left}, {self.op}, {self.right})'
 
-    # F
-    ('F', 'NUMBER'): ['NUMBER'],
-    ('F', 'IDENTIFIER'): ['IDENTIFIER'],
-    ('F', '('): ['(', 'E', ')'],
-}
+class ConditionalNode(ASTNode):
+    def __init__(self, condition, true_branch, false_branch):
+        self.condition = condition
+        self.true_branch = true_branch
+        self.false_branch = false_branch
 
-def get_production(nonterminal: str, terminal: str):
-    """
-    Lookup the production for (nonterminal, terminal).
-    Return a list of symbols (possibly empty for epsilon) or None if no entry exists.
-    Terminal should be token name like 'NUMBER' or symbol like '+'; use '$' for EOF.
-    """
-    return PARSE_TABLE.get((nonterminal, terminal))
+    def __repr__(self):
+        return f'ConditionalNode({self.condition}, {self.true_branch}, {self.false_branch})'
+
+class LetNode(ASTNode):
+    def __init__(self, identifier, value_expr, body_expr):
+        self.identifier = identifier
+        self.value_expr = value_expr
+        self.body_expr = body_expr
+    
+    def __repr__(self):
+        return f"Let({self.identifier}, {self.value_expr}, {self.body_expr})"
+
+class LambdaNode(ASTNode):
+    def __init__(self, parameter, body):
+        self.parameter = parameter
+        self.body = body
+    
+    def __repr__(self):
+        return f"Lambda({self.parameter}, {self.body})"
+
+class ApplicationNode(ASTNode):
+    def __init__(self, function, arguments):
+        self.function = function
+        self.arguments = arguments
+    
+    def __repr__(self):
+        return f"Application({self.function}, {self.arguments})"
+
+class LL1Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token_index = 0
+        self.current_token = tokens[0]
+    
+    def advance(self):
+        self.current_token_index += 1
+        if self.current_token_index < len(self.tokens):
+            self.current_token = self.tokens[self.current_token_index]
+    
+    def match(self, expected_type):
+        if self.current_token.type == expected_type:
+            value = self.current_token.value
+            self.advance()
+            return value
+        else:
+            raise SyntaxError(f"Expected {expected_type.name}, got {self.current_token.type.name}")
+        
+    def parse_expr(self):
+        pass
